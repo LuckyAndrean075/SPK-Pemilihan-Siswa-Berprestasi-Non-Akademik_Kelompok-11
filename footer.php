7351ad80f72597c29e6c6cfa5d3a1c7c97f3fbf1
LuckyAndrean075 <92172785+LuckyAndrean075@users.noreply.github.com>
2021-12-12 15:31:32 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,25 @@
+</div> <!-- /wrapper -->
+
+<footer>
+	 <font size="3">By Kelompok 11</font><br>
+</footer>
+
+<script src="js/jquery-3.2.1.js"></script>
+<script src="semantic/dist/semantic.min.js"></script>
+<script type="text/javascript">
+	$('.ui.radio.checkbox')
+		.checkbox()
+	;
+</script>
+<script type="text/javascript">
+	$('.message .close')
+	  .on('click', function() {
+	    $(this)
+	      .closest('.message')
+	      .transition('fade')
+	    ;
+	  })
+	;
+</script>
+</body>
+</html>
